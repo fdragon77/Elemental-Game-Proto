45b487c422b2e2bfbf9d9ff1c5a066c915bc4506
Unknown <u0398022@utah.edu>
2019-09-10 15:51:14 -0600

    added basic enemy movement

---
+++ Assets/Enemy Script/AttackPlayerInRange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayerInRange : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] float range;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(target.transform.position, transform.position) <= range)
+        {
+            transform.LookAt(target.transform);
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
