79b9fa5b4a5b42a95401598f648ee2f9ed314289
Filip <psycro77@gmail.com>
2019-09-10 13:46:56 -0600

    Adding Projectile Attack
    working on making a fireball, about to work on making it explode.

--- Assets/Scripts/Character_Control.cs
+++ Assets/Scripts/Character_Control.cs
@@ -9,6 +9,7 @@ public class Character_Control : MonoBehaviour
     public float speed = 6.0f;
     private float initSpeed;
     int time = 0;
+    public GameObject projectile;
     //public float gravity = 20.0f; not going to use gravity right now
 
     private Vector3 move = Vector3.zero;
@@ -25,7 +26,7 @@ public class Character_Control : MonoBehaviour
     void Update()
     {
         move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        if (Input.GetButtonDown("Jump")&& time <1)
+        if (Input.GetButtonDown("Jump") && time < 1)
         {
             Debug.Log("Dash");
             speed *= 4;
@@ -43,7 +44,25 @@ public class Character_Control : MonoBehaviour
                 speed = initSpeed;
             }
         }
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire");
+            GameObject fireballHandler;
+			fireballHandler = Instantiate (projectile, characterController.transform.position, characterController.transform.rotation) as GameObject;
+			//fireballHandler.transform.Rotate (Vector3.right * 90); 
+			Rigidbody TempBody;
+			TempBody = fireballHandler.GetComponent<Rigidbody>();
+            Vector3 fireDirection;
+            fireDirection = transform.forward;
+            fireDirection.x *= 800;
+            fireDirection.z *= 800;
+            fireDirection.y = 150;
+            
+            
+            Debug.Log(transform.forward.x +" "+ transform.forward.y +" "+ transform.forward.z );
+            TempBody.AddForce (fireDirection);
+        }
+
 
 
     }
