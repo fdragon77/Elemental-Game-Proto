aeecdd99b09842ba68d54c29c162d12fe347d939
Unknown <u0398022@utah.edu>
2019-09-09 22:21:54 -0600

    breath fire ability added.
    Its volatile at the moment.

---
+++ Assets/ability scripts/breathFire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breathFire : MonoBehaviour
+{
+    [SerializeField] fireBreath_stats breath;
+    public void cast()
+    {
+        Vector3 pos = gameObject.transform.position;
+        GameObject G = Instantiate(breath.gameObject, pos + (transform.forward * 1), gameObject.transform.rotation);
+        G.transform.rotation = gameObject.transform.rotation;
+    }
+}
+
