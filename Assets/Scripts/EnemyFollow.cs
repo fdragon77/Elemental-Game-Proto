c038fd3ce005bd35840606a639ad172a27135d02
Filip <psycro77@gmail.com>
2019-09-12 11:46:26 -0600

    Enemies abound, sort of
    added destructible objects, enemies that follow you relentlessly if you get too close, and a light for the elemental flame

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    GameObject player;
+    Transform playerLoc;
+    int MoveSpeed = 3;
+    int MaxDist = 20;
+    int MinDist = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerLoc = player.position;
+        player = GameObject.Find("Body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(player.transform.position);
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= MaxDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+    }
+}
