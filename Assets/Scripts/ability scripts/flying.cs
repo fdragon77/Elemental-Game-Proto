840d5f18bcc20d03cb40a02bb68b90471947c9e6
Unknown <u0398022@utah.edu>
2019-09-12 14:37:48 -0600

    tweaked flame breath

--- Assets/Scripts/ability scripts/flying.cs
+++ Assets/Scripts/ability scripts/flying.cs
@@ -6,15 +6,21 @@ public class flying : MonoBehaviour
 {
     [SerializeField] public float speed = 15;
     [SerializeField] public Vector3 direction;
+    [SerializeField] float maxDistance;
+    [HideInInspector] Vector3 startpos;
     // Start is called before the first frame update
     void Start()
     {
-
+        startpos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
+        if(Vector3.Distance(startpos, transform.position) > maxDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 }
