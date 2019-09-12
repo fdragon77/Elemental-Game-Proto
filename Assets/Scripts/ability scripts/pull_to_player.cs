1a9c9ea8f43f273120c85495b49fa9f8f8a0a4c5
Unknown <u0398022@utah.edu>
2019-09-12 13:13:05 -0600

    fixed a thing.

--- Assets/Scripts/ability scripts/pull_to_player.cs
+++ Assets/Scripts/ability scripts/pull_to_player.cs
@@ -6,10 +6,11 @@ public class pull_to_player : MonoBehaviour
 {
     [SerializeField] abilities ab;
     [SerializeField] float speed;
+    [SerializeField] float range;
     // Update is called once per frame
     void Update()
     {
-        if (ab.pull_healing)
+        if (ab.pull_healing && Vector3.Distance(transform.position, ab.gameObject.transform.position) <= range)
         {
             transform.position = Vector3.MoveTowards(transform.position, ab.gameObject.transform.position, speed * Time.deltaTime);
         }
