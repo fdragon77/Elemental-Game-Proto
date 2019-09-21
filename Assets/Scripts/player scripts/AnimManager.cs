ca622e7478b03ac66d1f275364138caf60aa869e
Filip <psycro77@gmail.com>
2019-09-21 15:22:24 -0600

    Melee works(sort of)
    melee attack is animated just need to add collision with enemies

---
+++ Assets/Scripts/player scripts/AnimManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimManager : MonoBehaviour
+{
+    private Animator theAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Swing");
+            theAnim.SetTrigger("Mouse Click");
+        }
+    }
+}
