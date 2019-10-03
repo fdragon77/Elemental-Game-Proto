9c9242b1e0dcde2a9a5d91f96a38f5e7ee82edb9
Filip <psycro77@gmail.com>
2019-10-03 15:12:12 -0600

    working on abilities

---
+++ Assets/Scripts/AbilityManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityManager : MonoBehaviour
+{
+    bool abil1bound = false;
+    bool abil2bound = false;
+    bool abil3bound = false;
+    bool abil4bound = false;
+    bool abil5bound = false;
+
+    float abil1cool;
+    float abil2cool;
+    float abil3cool;
+    float abil4cool;
+    float abil5cool;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
