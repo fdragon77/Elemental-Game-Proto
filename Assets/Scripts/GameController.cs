445fd1826f034d01f086a916f3179d41189676a0
Unknown <u0398022@utah.edu>
2019-10-03 14:36:46 -0600

    Added play button to main menu.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static float gamespeed = 1;
+    static GameController inst;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (inst == null)
+        {
+            inst = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadScene(string scn)
+    {
+        SceneManager.LoadScene(scn);
+    }
+}
