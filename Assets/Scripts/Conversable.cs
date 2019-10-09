ce7a0b28e3613a0441e73926069cecbe0a1f156e
Unknown <u0398022@utah.edu>
2019-10-09 16:06:44 -0600

    Basic Dialogue system implemented.
    I plan on making events work after different lines of dialogue. But, baby steps.

---
+++ Assets/Scripts/Conversable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.UI;
+using UnityEngine.UI;
+
+public class Conversable : MonoBehaviour
+{
+    [SerializeField] List<string> Speakers;
+    [SerializeField] List<string> Lines; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Converse()
+    {
+        for(int i = 0; i < Speakers.Count; i++)
+        {
+            UIPopup pop = UIPopup.GetPopup("dialogue1");
+            pop.Data.SetLabelsTexts(Speakers[i], Lines[i]);
+            UIPopupManager.AddToQueue(pop);
+        }
+    }
+
+    public void Update()
+    {
+        //For testing
+        if (Input.GetKeyDown(KeyCode.C) && GameController.gamespeed > 0)
+        {
+            Converse();
+        }
+    }
+}
