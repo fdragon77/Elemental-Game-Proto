9f112512ef70049fdb3fa49250371e312fd0c69a
Jake D <u0398022@utah.edu>
2019-10-21 19:05:53 -0600

    Things can be destroyed now!

---
+++ Assets/Editor/DestrucableEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Destructable))]
+[CanEditMultipleObjects]
+public class DestrucableEditor : Editor
+{
+    //Editor only vars
+    bool ShowDestroyObjects = true;
+
+    //Stuff from the original
+    SerializedProperty FireballsDestroy;
+    SerializedProperty FirebreathsDestroy;
+    SerializedProperty BeamDestroy;
+    SerializedProperty HealDestroy;
+    SerializedProperty AOEDestroy;
+    SerializedProperty TargetedAOEDestroy;
+    SerializedProperty DestructionType;
+    SerializedProperty burnTexture;
+    SerializedProperty explodeObj;
+
+    private void OnEnable()
+    {
+        FireballsDestroy = serializedObject.FindProperty("FireballsDestroy");
+        FirebreathsDestroy = serializedObject.FindProperty("FirebreathsDestroy");
+        BeamDestroy = serializedObject.FindProperty("BeamDestroy");
+        HealDestroy = serializedObject.FindProperty("HealDestroy");
+        AOEDestroy = serializedObject.FindProperty("AOEDestroy");
+        TargetedAOEDestroy = serializedObject.FindProperty("TargetedAOEDestroy");
+        DestructionType = serializedObject.FindProperty("DestructionType");
+        burnTexture = serializedObject.FindProperty("burnTexture");
+        explodeObj = serializedObject.FindProperty("explodeObj");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public enum destroyType {normal, explode, burn};
+
+    [Header("Destructable types")]
+    [SerializeField] bool FireballsDestroy;
+    [SerializeField] bool FirebreathsDestroy;
+    [SerializeField] bool BeamDestroy;
+    [SerializeField] bool HealDestroy;
+    [SerializeField] bool AOEDestroy;
+    [SerializeField] bool TargetedAOEDestroy;
+
+    [Header("How Does this become destroyed?")]
+    [SerializeField] destroyType DestructionType = destroyType.normal;
+
+    [SerializeField] Material burnmat;
+    [SerializeField] GameObject explodeObj;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Attack")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Fireball(Clone)":
+                    if (FireballsDestroy)
+                    {
+                        destruct();
+                    }
+                    break;
+                //FIXME Fill in with prefabs as we work on it.
+            }
+        }
+    }
+
+    /// <summary>
+    /// Trigger events of destroying the object.
+    /// </summary>
+    private void destruct()
+    {
+        switch (DestructionType)
+        {
+            case destroyType.normal:
+                Destroy(gameObject);
+                break;
+            case destroyType.explode:
+                Destroy(gameObject);
+                break;
+            case destroyType.burn:
+                gameObject.GetComponent<MeshRenderer>().material = burnmat;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// What happens when object is actually destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        switch (DestructionType)
+        {
+            case destroyType.normal:
+                break;
+            case destroyType.explode:
+                Vector3 pos = transform.position;
+                Instantiate(explodeObj, pos, new Quaternion());
+                break;
+            case destroyType.burn:
+                break;
+        }
+    }
+}
