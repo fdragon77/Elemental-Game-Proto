f18a32d55b0d37dae19121314d404d867980f951
Filip <psycro77@gmail.com>
2019-10-24 19:10:52 -0600

    Aoe on ability instead of enemies now
    just add the knockback ability script to aany ability you want to send things flying with

---
+++ Assets/Scripts/Ability Scripts/KnockbackAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockbackAbility : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        Debug.Log("Enter abil");
+        Debug.Log(trigger.gameObject.tag);
+
+        if (trigger.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Force abil");
+            Vector3 movedirection = transform.position - trigger.transform.position;
+            Rigidbody theBody = trigger.GetComponent<Rigidbody>();
+            Debug.Log(theBody.name);
+            Debug.Log(movedirection.normalized);
+            theBody.AddForce(movedirection.normalized * -5000);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
