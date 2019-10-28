da94a07f7fa8b7f1e84a4500f9d821c95e0dcd1c
Will <Willgroves91@gmail.com>
2019-10-27 21:07:38 -0600

    hud up try again

---
+++ Assets/hudanimate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class hudanimate : MonoBehaviour
+{
+    private RawImage barRawImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        barRawImage = transform.Find("mana bar fill").GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect uvRect = barRawImage.uvRect;
+        uvRect.x -= .3f * Time.deltaTime;
+        barRawImage.uvRect = uvRect;
+
+    }
+}
