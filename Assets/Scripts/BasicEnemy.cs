359da6d2a3dd9de390491f128375ecd9dea6d151
Filip <psycro77@gmail.com>
2019-11-10 19:51:38 -0700

    figuring out dependencies and enemies

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    GameObject player;
+    Transform playerLoc;
+    int MoveSpeed = 3;
+    float MaxDist = 20f;
+    float MinDist = 2f;
+    float yLoc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerLoc = player.position;
+        player = GameObject.Find("ElementalPlayer");
+        yLoc = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+
+        if ((Vector3.Distance(transform.position, player.transform.position) <= MaxDist) && (Vector3.Distance(transform.position, player.transform.position) >= MinDist))
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+    }
+}
+
