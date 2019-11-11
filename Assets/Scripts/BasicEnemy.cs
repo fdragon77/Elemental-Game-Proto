166000581bdb113908029763bcd8fa34e29f1091
Filip <psycro77@gmail.com>
2019-11-10 21:19:34 -0700

    working on enemy attack

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -7,9 +7,14 @@ public class BasicEnemy : MonoBehaviour
     GameObject player;
     Transform playerLoc;
     int MoveSpeed = 3;
-    float MaxDist = 20f;
-    float MinDist = 2f;
+    float MaxDist = 30f;
+    float MinDist = 5f;
+    float attackRange = 10f;
     float yLoc;
+
+    public GameObject projectile;
+    float timer = 0;
+    //bool canShoot = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +23,41 @@ public class BasicEnemy : MonoBehaviour
         yLoc = transform.position.y;
     }
 
+    public void Fire()
+    {
+        Debug.Log("EnemyAttack");
+        
+
+        GameObject projectileHandler;      
+     
+        projectileHandler = Instantiate(projectile, transform.position, projectile.transform.rotation) as GameObject;
+
+        float projectileSpeed = 20f;
+        float projectileHeight = .1f;
+        //projectileHandler.GetComponent<Rigidbody>().velocity = projectile.transform.TransformDirection(fireDirection.x * projectileSpeed, fireDirection.y * projectileHeight, fireDirection.z * projectileSpeed);
+        
+
+
+
+
+    }
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(player.transform.position);
-
+        timer -= Time.deltaTime;
         if ((Vector3.Distance(transform.position, player.transform.position) <= MaxDist) && (Vector3.Distance(transform.position, player.transform.position) >= MinDist))
         {
 
             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
             //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        
+        if (timer <= 0 && (Vector3.Distance(transform.position, player.transform.position) <= attackRange))
+        {
+
+            Fire();
+            timer = 4.0f;
         }
     }
 }
