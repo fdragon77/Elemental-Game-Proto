654cf2ba6710a64969a9f1abef05fd26999aba30
Jake Elliott <marrowofbone@gmail.com>
2019-11-21 14:35:33 -0700

    fixed I think

---
+++ Assets/Scripts/destroytimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroytimer : MonoBehaviour
+{
+    [SerializeField] float countdownTime = 2f;
+    private float starttime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= starttime + countdownTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
