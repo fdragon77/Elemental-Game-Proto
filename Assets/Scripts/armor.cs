aea2ad148b22da36c6877fa115d4ad18f1bfe697
Will <Willgroves91@gmail.com>
2019-11-25 10:32:34 -0700

    armor_stuff file with armor bar sprites
    armor script to adjust armor bar sprites with health

---
+++ Assets/Scripts/armor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class armor : MonoBehaviour
+{
+
+    private int health = 100;
+    public Text healthText;
+    public Sprite armor1;
+    Sprite[] armorSprites;
+    private void Awake()
+    {
+        LoadAllSprites();
+        //this.gameObject.GetComponent<SpriteRenderer>().sprite = (armor1);
+        
+
+    }
+
+    void Update()
+    {
+        healthText.text = "Health : " + health;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+
+            armor1 = armorSprites[100 - health];
+
+        }
+        this.gameObject.GetComponent<Image>().sprite = (armor1);
+       
+    }
+
+   
+    void LoadAllSprites()
+    {
+
+        armorSprites = Resources.LoadAll<Sprite>("armor_stuff");
+       
+        
+
+        armor1 = armorSprites[100 - health];
+        
+        foreach (Sprite s in armorSprites)
+        {
+            Debug.Log(s.name);
+            
+        }
+    
+
+
+    }
+}
