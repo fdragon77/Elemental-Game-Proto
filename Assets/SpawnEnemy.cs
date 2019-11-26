197235fe124103c693267cfa56816de7e40e6d11
Filip <psycro77@gmail.com>
2019-11-26 12:10:30 -0700

    toggleEnemies

---
+++ Assets/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject toSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+
+
+        if ((collision.gameObject.tag == "Player"))
+        {
+            toSpawn.SetActive(true);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
