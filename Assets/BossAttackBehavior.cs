12874c4365839d1bbf8d1d40af15239756fe3432
Filip <psycro77@gmail.com>
2019-12-26 21:43:07 -0700

    really bad memory intensive attack

---
+++ Assets/BossAttackBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackBehavior : MonoBehaviour
+{
+    //We will need a trigger for this eventually. 
+    [SerializeField] float max_distance = 50f;
+    [SerializeField] float speed = 1;
+    [SerializeField] float lifetime = 10f;
+    [SerializeField] GameObject theVisual;
+    [HideInInspector] GameObject player;
+    List<GameObject> theAttacks = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        GameObject visualHandler;
+        Vector3 startPos = transform.position;
+        visualHandler = Instantiate(theVisual, startPos, theVisual.transform.rotation) as GameObject;
+
+        theAttacks.Add(visualHandler);
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Boom();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == player.gameObject.tag)
+        {
+            Boom();
+        }
+    }
+
+    private void Boom()
+    {
+        Debug.Log(theAttacks.Count);
+        foreach (GameObject theObject in theAttacks)
+        {
+            Debug.Log("Delete Me");
+            Destroy(theObject);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/BossTestScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTestScript : MonoBehaviour
+{
+    GameObject player;
+    Transform playerLoc;
+    [SerializeField] int MoveSpeed;
+    [SerializeField] float MaxDist;
+    [SerializeField] float MinDist;
+    [SerializeField] float attackRange;
+    [SerializeField] float projAdj;
+    float yLoc;
+
+    public GameObject projectile;
+    public GameObject bossAttack;
+    float timer = 0;
+    //bool canShoot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerLoc = player.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        yLoc = transform.position.y;
+    }
+
+    public void Fire()
+    {
+        Debug.Log("EnemyAttack");
+        float outcome = Random.Range(-100f, 100f);
+        if (outcome <= 0)
+        {
+            RockThrow();
+        }
+        else
+        {
+            BossAttack();
+        }
+    }
+    public void TargetAcquire()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log("Target Acquired");
+        Debug.Log(player);
+    }
+
+    private void RockThrow()
+    {
+        GameObject projectileHandler;
+        Vector3 startPos = transform.position;
+        startPos.y += projAdj;
+        projectileHandler = Instantiate(projectile, startPos, projectile.transform.rotation) as GameObject;
+
+        float projectileSpeed = 30f;
+        float projectileHeight = .1f;
+        Vector3 aim;
+        aim = player.transform.position - transform.position;
+        aim *= .1f;
+        aim.Normalize();
+        projectileHandler.GetComponent<Rigidbody>().velocity = projectile.transform.TransformDirection(aim.x * projectileSpeed, aim.y * projectileHeight, aim.z * projectileSpeed);
+    }
+
+    private void BossAttack()
+    {
+        GameObject attackHandler;
+        Vector3 startPos = transform.position;
+        startPos.y += projAdj;
+        attackHandler = Instantiate(bossAttack, startPos, bossAttack.transform.rotation) as GameObject;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+        timer -= Time.deltaTime;
+        if ((Vector3.Distance(transform.position, player.transform.position) <= MaxDist) && (Vector3.Distance(transform.position, player.transform.position) >= MinDist))
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+
+        if (timer <= 0 && (Vector3.Distance(transform.position, player.transform.position) <= attackRange))
+        {
+
+            Fire();
+            timer = 4.0f;
+        }
+        
+    }
+}
