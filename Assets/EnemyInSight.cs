21229840b4556743f68607c5430236f1d90aaad1
Filip <psycro77@gmail.com>
2020-01-21 15:21:01 -0700

    rudimentary lock on

--- Assets/EnemyInSight.cs
+++ Assets/EnemyInSight.cs
@@ -6,30 +6,33 @@ public class EnemyInSight : MonoBehaviour
 {
     Camera theCamera;
     bool beenAdded = false;
-    bool visible = false;
+    bool visible = true;
     // Start is called before the first frame update
     void Start()
     {
-        //theCamera = GameObject.FindObjectWithTag("MainCamera");
+        theCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        Vector3 EnemyPos = theCamera.WorldToViewportPoint(gameObject.transform.position);
+        
+        /*Vector3 EnemyPos = theCamera.WorldToViewportPoint(gameObject.transform.position);
 
         if (EnemyPos.z > 0 && EnemyPos.x > 0 && EnemyPos.z < 1 && EnemyPos.x < 1)
         {
             visible = true;
+            Debug.Log("I see you");
         }
         else
             visible = false;
+            */
         if(visible && !beenAdded)
         {
             beenAdded = true;
-            //add to list TargetLock.nearbyEnemies.add(gameobject);
-        }*/
+           
+            TargetLock.targetableEnemies.Add(gameObject);
+        }
     }
 }
--- Assets/TargetLock.cs
+++ Assets/TargetLock.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetLock : MonoBehaviour
 {
     [SerializeField] Camera PlayerCam;
     GameObject lockedEnemyOb;
-    //Image crosshair;
+    [SerializeField] Image crosshair;
 
     bool lockedOn;
-
-    int lockedEnemyList;
+    bool beenLocked;
+    int lockedEnemy;
 
     public static List<GameObject> targetableEnemies = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        //PlayerCam = GameObject.FindObjectWithTag("MainCamera");
+        PlayerCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        lockedOn = false;
+        lockedEnemy = 0;
 
     }
 
@@ -24,5 +27,78 @@ public class TargetLock : MonoBehaviour
     void Update()
     {
         
+        if (Input.GetButtonDown("LockOn") && !lockedOn)
+        {
+            Debug.Log("LockOn");
+            if (targetableEnemies.Count >= 1)
+            {
+                lockedOn = true;
+                crosshair.enabled = true;
+                if (!beenLocked || lockedEnemyOb == null)
+                {
+                    //Lock On To First Enemy In List By Default
+                    lockedEnemy = 0;
+                    lockedEnemyOb = targetableEnemies[lockedEnemy];
+                    beenLocked = true;
+                }               
+            }           
+        }
+        else if (Input.GetButtonDown("LockOn") && lockedOn)
+        {
+            lockedOn = false;
+            crosshair.enabled = false;
+            //lockedEnemy = 0;
+            //lockedEnemyOb = null;
+        }
+
+        if (lockedOn && lockedEnemyOb == null)
+        {
+            lockedOn = false;
+            crosshair.enabled = false;
+            targetableEnemies.Remove(lockedEnemyOb);
+        }
+        if (Input.GetButtonDown("SwitchLeft")&& lockedOn)
+        {
+            if (lockedEnemy == 0)
+            {
+                //If End Of List Has Been Reached, Start Over
+                lockedEnemy = targetableEnemies.Count - 1;
+                lockedEnemyOb = targetableEnemies[lockedEnemy];
+            }
+            else
+            {
+                //Move To Next Enemy In List
+                lockedEnemy--;
+                lockedEnemyOb = targetableEnemies[lockedEnemy];
+            }
+        }
+        if (Input.GetButtonDown("SwitchRight") && lockedOn)
+        {
+            if (lockedEnemy == targetableEnemies.Count - 1)
+            {
+                //If End Of List Has Been Reached, Start Over
+                lockedEnemy = 0;
+                lockedEnemyOb = targetableEnemies[lockedEnemy];
+            }
+            else
+            {
+                //Move To Next Enemy In List
+                lockedEnemy++;
+                lockedEnemyOb = targetableEnemies[lockedEnemy];
+            }
+        }
+
+        if (lockedOn && lockedEnemyOb != null)
+        {
+            lockedEnemyOb = targetableEnemies[lockedEnemy];
+
+            //Determine Crosshair Location Based On The Current Target
+            gameObject.transform.position = PlayerCam.WorldToScreenPoint(lockedEnemyOb.transform.position);
+
+            //Rotate Crosshair
+            gameObject.transform.Rotate(new Vector3(0, 0, -1));
+        }
+        
+
     }
 }
