00a3da53ac4861ea9d91d503fe2d1b19f454eca2
Jake D <u0398022@utah.edu>
2020-01-25 10:25:08 -0700

    Moving platform script is in!

---
+++ Assets/Scripts/movingEnviroment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingEnviroment : MonoBehaviour
+{
+    [SerializeField] Vector3 Goal;
+    [SerializeField] Vector3 ResetZone;
+    [SerializeField] float speed = 3;
+    [SerializeField] float tollerance = 0.1f;
+    [SerializeField] bool wrap = true; //true to warp back to start, false to start moving back to start. 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Goal, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, Goal) <= tollerance && wrap)
+        {
+            transform.position = ResetZone;
+        }
+        else if (Vector3.Distance(transform.position, Goal) <= tollerance && !wrap)
+        {
+            Vector3 temp = ResetZone;
+            ResetZone = Goal;
+            Goal = temp;
+        }
+    }
+}
