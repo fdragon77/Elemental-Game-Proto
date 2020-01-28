a491791608326c01ac083e67898ed71c3b3231c8
Will <Willgroves91@gmail.com>
2020-01-27 23:19:15 -0700

    human movement scripts

---
+++ Assets/humanMOVE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanMOVE : MonoBehaviour
+{
+    public Transform[] target;
+    public Transform dirTarget;
+    public float speed;
+    private int current;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = dirTarget.position - transform.position ;
+        Quaternion rotation = Quaternion.LookRotation(direction );
+        transform.rotation = rotation  ;
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+
+        }
+    
+
+}
