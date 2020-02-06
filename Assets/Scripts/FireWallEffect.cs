ee41a3da34c3ce192d73ba760e82a488fa032964
Jake D <u0398022@utah.edu>
2020-02-06 11:51:52 -0700

    Firewall is feeling much nicer!

--- Assets/Scripts/FireWallEffect.cs
+++ Assets/Scripts/FireWallEffect.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class FireWallEffect : MonoBehaviour
 {
     [SerializeField] GameObject WallObject;
+    [SerializeField] float activateTime;
+    private float starttime;
+    private BoxCollider Box;
     GameObject Blast;
     // Start is called before the first frame update
     void Start()
     {
-
         Blast = Instantiate(WallObject, new Vector3(transform.position.x, transform.position.y-4, transform.position.z), WallObject.transform.rotation) as GameObject;
+        starttime = Time.time;
+        Box = gameObject.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time - starttime >= activateTime)
+        {
+            Box.enabled = true;
+        }
     }
 }
