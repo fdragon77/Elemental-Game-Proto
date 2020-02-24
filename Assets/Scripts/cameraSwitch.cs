a1454da55d9890d40fb0b8b02b6c51bf882a4376
Jake D <u0398022@utah.edu>
2020-02-23 20:30:40 -0700

    Boss camera for fracture.

---
+++ Assets/Scripts/cameraSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject onCamera;
+    [SerializeField] GameObject offCamera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            onCamera.SetActive(true);
+            offCamera.SetActive(false);
+        }
+    }
+
+}
