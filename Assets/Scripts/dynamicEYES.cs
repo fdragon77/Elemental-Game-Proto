77812483a01e9a75486147ef9c716a3b3fd0055f
Will <Willgroves91@gmail.com>
2020-02-27 11:56:20 -0700

    attempt at eye movement

---
+++ Assets/Scripts/dynamicEYES.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditorInternal;
+using UnityEditor;
+
+
+public class dynamicEYES : MonoBehaviour
+{
+  
+    public bool preview = true;
+    public float maxZFar = -5;
+    public List<Eye> Eyes = new List<Eye>();
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        foreach (Eye a in Eyes)
+        {
+            if (a.eye != null) Gizmos.DrawWireSphere(a.eye.position, a.radius);
+        }
+    }
+
+    void Update()
+    {
+        DrawEyes();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (preview) DrawEyes();
+    }
+
+    void DrawEyes()
+    {
+        foreach (Eye a in Eyes)
+        {
+            Vector2 controller;
+            float lerpVal = (maxZFar - transform.position.z) / maxZFar;
+            controller = Vector2.Lerp(a.eye.localPosition, transform.localPosition, lerpVal / 2);
+
+            var leftPos = Vector2.ClampMagnitude(controller - (Vector2)a.eye.localPosition, a.radius);//Normalization
+            if (Quaternion.Angle(a.eye.rotation, transform.rotation) > 60)
+            {
+                leftPos.x *= -1;
+            }
+
+            a.eye.GetChild(0).localPosition = leftPos;//The first child is the pupil 
+        }
+    }
+}
+
+[System.Serializable]
+public struct Eye
+{
+    public Transform eye;
+    public float radius;
+}
