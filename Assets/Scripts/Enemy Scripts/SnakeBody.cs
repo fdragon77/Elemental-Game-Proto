70bc5e2399fa26d6340d16447d4e950086692954
Jake D <u0398022@utah.edu>
2020-04-01 15:23:42 -0600

    Snake boss idea in progress.

---
+++ Assets/Scripts/Enemy Scripts/SnakeBody.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBody : MonoBehaviour
+{
+    [SerializeField] private SnakeHead Head;
+    private float speed;
+    private float rotateSpeed;
+    [SerializeField] private GameObject Follow;
+    private Vector3 go;
+    [SerializeField] private float updatetime = .1f;
+    private float startTime;
+    [SerializeField] private float socialDistance = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Head.speed;
+        rotateSpeed = 1;
+        startTime = Time.time;
+        go = Follow.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.gamespeed > 0)
+        {
+            if (Follow == null)
+            {
+                Destroy(gameObject);
+            }
+            if (Vector3.Distance(transform.position, Follow.transform.position) >= socialDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, go, speed * Time.deltaTime);
+            }
+            //Quaternion TargetQ = Quaternion.LookRotation(go - transform.position);
+            //transform.localRotation = Quaternion.Lerp(transform.rotation, TargetQ, rotateSpeed * Time.deltaTime);
+
+            if (Time.time >= startTime + updatetime)
+            {
+                startTime = Time.time;
+                go = Follow.transform.position;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Head")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/SnakeHead.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+    private GameObject Player;
+    private GameObject Target;
+
+    [SerializeField] public float speed;
+    [SerializeField] public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Target = Player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.gamespeed > 0)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            Quaternion TargetQ = Quaternion.LookRotation(Target.transform.position - transform.position);
+            TargetQ.z = transform.rotation.z;
+            transform.localRotation = Quaternion.Lerp(transform.rotation, TargetQ, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
