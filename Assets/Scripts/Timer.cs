2fc58098e020d69df340761444b534a1887443c6
ktique <u1160240@utah.edu>
2020-04-07 15:01:21 -0600

    added a basic countdown timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject textDisplay;
+    public int secondsLeft = 30;
+    public bool takingAway = false;
+
+    void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+    }
+
+    void Update()
+    {
+        if (takingAway == false && secondsLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+    IEnumerator TimerTake() 
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        if (secondsLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondsLeft;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+        }
+        
+        takingAway = false;
+    }
+
+
+}
