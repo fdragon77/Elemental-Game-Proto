772e3d985082fbc1c0efe95224fb0f868fc8c9a7
Jake D <u0398022@utah.edu>
2020-04-09 15:27:44 -0600

    Torches in to distract the snake.

---
+++ Assets/Scripts/Enemy Scripts/snakeTorches.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Destructable))]
+public class snakeTorches : MonoBehaviour
+{
+    [SerializeField] private float burnTime = 5f;
+    [SerializeField] private GameObject burnEffect;
+    public bool burning = false;
+    private float StartBurningTime;
+    [SerializeField] private UnityEvent burnEvent;
+    [SerializeField] private UnityEvent StopEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartBurningTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.gamespeed > 0)
+        {
+            burnEffect.SetActive(burning);
+            if (Time.time >= StartBurningTime + burnTime)
+            {
+                burning = false;
+                StopEvent.Invoke();
+            }
+        }
+    }
+
+    public void burn()
+    {
+        StartBurningTime = Time.time;
+        burning = true;
+        burnEvent.Invoke();
+    }
+}
